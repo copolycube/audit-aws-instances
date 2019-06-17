f261a6021e2e1a30d37382e9f5690744cac44545
Copolycube <kaourintin+github@gmail.com>
2019-06-17 12:37:06 -0400

    Update audit-aws-instances.sh

--- audit-aws-instances.sh
+++ audit-aws-instances.sh
@@ -1,58 +1,61 @@
 #!/bin/bash
 
-###########################################
-# Audits AWS instances for unused reserved
-# instances and instances which do not have
-# corresponding reserved instances.
-###########################################
+aws_regions=$(aws ec2 describe-regions --query "Regions[].{Name:RegionName}" --output text --region ca-central-1)
+for region in $aws_regions; do
+    echo "---------- AWS Region : $region ----------" ; 
+    ###########################################
+    # Audits AWS instances for unused reserved
+    # instances and instances which do not have
+    # corresponding reserved instances.
+    ###########################################
 
-# Critical (exit code 2) if > this number
-crit=4
+    # Critical (exit code 2) if > this number
+    crit=4
 
-used_types="m3.medium m3.large m3.xlarge m3.2xlarge m4.large m4.xlarge m4.2xlarge m4.4xlarge r3.large r3.xlarge r3.2xlarge r3.4xlarge r3.8xlarge"
+    used_types=$(/usr/local/bin/aws ec2 describe-reserved-instances --region $region --filter Name=state,Values=active Name=scope,Values=Region --output text | grep RESERVEDINSTANCES | awk '{print $8}' |sort|uniq)
 
-source ~/.profile
+    active_res=$(/usr/local/bin/aws ec2 describe-reserved-instances --filter Name=state,Values=active Name=scope,Values="Availability Zone" --output text --region $region)
+    active_conv=$(/usr/local/bin/aws ec2 describe-reserved-instances --filter Name=state,Values=active Name=scope,Values=Region --output text --region $region)
+    active_ins=$(/usr/local/bin/aws ec2 describe-instances --filter Name=instance-state-name,Values=running --output text --region $region)
 
-active_res=$(/usr/local/bin/aws ec2 describe-reserved-instances --filter Name=state,Values=active Name=scope,Values="Availability Zone" --output text)
-active_conv=$(/usr/local/bin/aws ec2 describe-reserved-instances --filter Name=state,Values=active Name=scope,Values=Region --output text)
-active_ins=$(/usr/local/bin/aws ec2 describe-instances --filter Name=instance-state-name,Values=running --output text)
+    exit_code=0
 
-exit_code=0
+    # Loop through reserved instances
 
-# Loop through reserved instances
+    for type in $used_types; do
+	res_total=0
+	ins_total=0
+	while read -r instance; do
+	    qty=$(echo "$instance" | awk '{print $7}')
+	    res_total=$(expr "$res_total" + "$qty")
+	done < <(echo "$active_res" | grep "$type")
+	while read -r instance; do
+	    qty=$(echo "$instance" | awk '{print $6}')
+	    res_total=$(expr "$res_total" + "$qty")
+	done < <(echo "$active_conv" | grep "$type")
+	ins_total=$(echo "$active_ins" | grep -c "$type")
+	diff=$(($res_total - $ins_total))
+	if [[ $diff -gt $crit ]]; then
+	    echo "$region UNUSED RESERVATION: (${diff}) - $type"
+	    exit_code=2
+	elif [[ $diff -gt 0 ]]; then
+	    echo "$region UNUSED RESERVATION: (${diff}) - $type"
+	    if [[ $exit_code -ne 2 ]]; then
+		exit_code=1
+	    fi
+	elif [[ $diff -lt -${crit} ]]; then
+	    diff=$(echo $diff | sed 's/-//g')
+	    echo "$region UNRESERVED INSTANCES: (${diff}) - $type"
+	    exit_code=2
+	elif [[ $diff -lt 0 ]]; then
+	    diff=$(echo $diff | sed 's/-//g')
+	    echo "$region UNRESERVED INSTANCES: (${diff}) - $type"
+	    if [[ $exit_code -ne 2 ]]; then
+		exit_code=1
+	    fi
+	fi
+    done
 
-for type in $used_types; do
-  res_total=0
-  ins_total=0
-  while read -r instance; do
-    qty=$(echo "$instance" | awk '{print $7}')
-    res_total=$(expr "$res_total" + "$qty")
-  done < <(echo "$active_res" | grep "$type")
-  while read -r instance; do
-    qty=$(echo "$instance" | awk '{print $6}')
-    res_total=$(expr "$res_total" + "$qty")
-  done < <(echo "$active_conv" | grep "$type")
-  ins_total=$(echo "$active_ins" | grep -c "$type")
-  diff=$(($res_total - $ins_total))
-  if [[ $diff -gt $crit ]]; then
-    echo "UNUSED RESERVATION: (${diff}) - $type"
-    exit_code=2
-  elif [[ $diff -gt 0 ]]; then
-    echo "UNUSED RESERVATION: (${diff}) - $type"
-    if [[ $exit_code -ne 2 ]]; then
-      exit_code=1
-    fi
-  elif [[ $diff -lt -${crit} ]]; then
-    diff=$(echo $diff | sed 's/-//g')
-    echo "UNRESERVED INSTANCES: (${diff}) - $type"
-    exit_code=2
-  elif [[ $diff -lt 0 ]]; then
-    diff=$(echo $diff | sed 's/-//g')
-    echo "UNRESERVED INSTANCES: (${diff}) - $type"
-    if [[ $exit_code -ne 2 ]]; then
-      exit_code=1
-    fi
-  fi
+#    exit $exit_code
+
 done
-
-exit $exit_code
